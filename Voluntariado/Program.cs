bba69ca41991b8798c3e73c27a2a4bcac119962b
More <gaitanmorena47@gmail.com>
2026-06-04 14:52:57 -0300

    Entidades

---
+++ Voluntariado/Program.cs
@@ -0,0 +1,5 @@
+﻿/*sistema de voluntariado
+desarrollar un sistema de consola, que permita administrar personas de una organización solidaria 
+deben existir, voluntarios y coordinadores . los voluntarios deberán mostrar cantidad de horas 
+trabajadas y los coordinadores áreas asignadas y cantidad de personas a cargo.
+ Aplicar obligatoriamente todo*/
